84d000d0cbb3d871dab1cb9c718e41e6e7f31ad0
bbakernoaa <barry@Barrys-MacBook-Pro.local>
2019-03-04 12:13:46 -0500

    time updates

--- fv3_pair_aeronet.py
+++ fv3_pair_aeronet.py
@@ -1,93 +1,138 @@
 #!/usr/bin/env python
 
-__author__  = 'Patrick Campbell'
-__email__   = 'patrick.c.campbell@noaa.gov'
+__author__ = 'Patrick Campbell'
+__email__ = 'patrick.c.campbell@noaa.gov'
 __license__ = 'GPL'
-
-
 
 #Simple MONET utility to command line pair model vs. observations
 
 import os
-from glob import glob
-import sys
-
 import subprocess
+import sys
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from distutils.spawn import find_executable
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from glob import glob
 
-import monet  
-from monet.util.tools import long_to_wide
 import pandas as pd
 
-def  pair_point(da,df,sub_map,interp):
-     dfpair=da.monet.combine_point(df,mapping_table=sub_map,method=interp)
-#     print(dfpair)
-     return dfpair
+import monet
+from monet.util.tools import long_to_wide
 
-def  get_aeronet(start,end):
-     dates = pd.date_range(start=start, end=end, freq='H')
-     df = monet.obs.aeronet.add_data(dates,freq='H')
-     return df.dropna(subset=['latitude','longitude'])
 
-def  open_cmaq(finput,verbose=False):
-     dset=monet.models.fv3chem.open_mfdataset(finput)
-     return dset
+def pair_point(da, df, sub_map, interp):
+    dfpair = da.monet.combine_point(
+        df, mapping_table=sub_map, method=interp, cleanup=True)
+    #     print(dfpair)
+    return dfpair
+
+
+def get_aeronet(start, end):
+    dates = pd.date_range(start=start, end=end, freq='H')
+    df = monet.obs.aeronet.add_data(dates, freq='H')
+    return df.dropna(subset=['latitude', 'longitude'])
+
+
+def open_cmaq(finput, verbose=False):
+    dset = monet.models.fv3chem.open_mfdataset(finput)
+    return dset
 
 
 if __name__ == '__main__':
 
-    parser = ArgumentParser(description='pairs cmaq model data to aqs observations', formatter_class=ArgumentDefaultsHelpFormatter)
-    
-    parser.add_argument('-f', '--files',       help='string input model file directory/names', type=str, required=True)
-#    parser.add_argument('-s', '--startdates',  help='string input start date for pairing YYYY-MM-DD HH:MM:SS', type=str, required=True)
-#    parser.add_argument('-e', '--enddates',    help='string input end date for pairing YYYY-MM-DD HH:MM:SS', type=str, required=True)
-    parser.add_argument('-x', '--species',     help='string input for obs species-variables to pair',type=str,nargs='+', required=False, default=['OZONE','PM2.5'])
-    parser.add_argument('-o', '--output',      help='string output path for paired dataframe, stats, plots', type=str, required=False,default='paired_output.hdf')
-    parser.add_argument('-p', '--path',        help='string path to director of network observations', type=str, required=False, default='/data/aqf2/barryb/5xpm/AQS_DATA/')
-    parser.add_argument('-n', '--network',     help='string input data network name: airnow, aqs', type=str, required=False, default='airnow')
-    parser.add_argument('-m', '--model',       help='input model: cmaq, fv3, hysplit (not-ready), or camx (not-ready)', type=str, required=False, default='cmaq')
-    parser.add_argument('-i', '--interp',      help='xesmf interpolation scheme, bilinear, conservative, nearest_s2d, nearest_d2s, patch', type=str, required=False, default='bilinear')
-    parser.add_argument('-v', '--verbose',     help='print debugging information', action='store_true', required=False)
+    parser = ArgumentParser(
+        description='pairs cmaq model data to aqs observations',
+        formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        '-f',
+        '--files',
+        help='string input model file directory/names',
+        type=str,
+        required=True)
+    #    parser.add_argument('-s', '--startdates',  help='string input start date for pairing YYYY-MM-DD HH:MM:SS', type=str, required=True)
+    #    parser.add_argument('-e', '--enddates',    help='string input end date for pairing YYYY-MM-DD HH:MM:SS', type=str, required=True)
+    parser.add_argument(
+        '-x',
+        '--species',
+        help='string input for obs species-variables to pair',
+        type=str,
+        nargs='+',
+        required=False,
+        default=['OZONE', 'PM2.5'])
+    parser.add_argument(
+        '-o',
+        '--output',
+        help='string output path for paired dataframe, stats, plots',
+        type=str,
+        required=False,
+        default='paired_output.hdf')
+    parser.add_argument(
+        '-p',
+        '--path',
+        help='string path to director of network observations',
+        type=str,
+        required=False,
+        default='/data/aqf2/barryb/5xpm/AQS_DATA/')
+    parser.add_argument(
+        '-n',
+        '--network',
+        help='string input data network name: airnow, aqs',
+        type=str,
+        required=False,
+        default='airnow')
+    parser.add_argument(
+        '-m',
+        '--model',
+        help='input model: cmaq, fv3, hysplit (not-ready), or camx (not-ready)',
+        type=str,
+        required=False,
+        default='cmaq')
+    parser.add_argument(
+        '-i',
+        '--interp',
+        help=
+        'xesmf interpolation scheme, bilinear, conservative, nearest_s2d, nearest_d2s, patch',
+        type=str,
+        required=False,
+        default='bilinear')
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        help='print debugging information',
+        action='store_true',
+        required=False)
     args = parser.parse_args()
 
-    finput  = args.files
-#    start   = args.startdates
-#    end     = args.enddates 
+    finput = args.files
+    #    start   = args.startdates
+    #    end     = args.enddates
     species = args.species
-    output  = args.output
-    datapath= args.path
+    output = args.output
+    datapath = args.path
     network = args.network
-    model   = args.model
-    interp  = args.interp
+    model = args.model
+    interp = args.interp
     verbose = args.verbose
     #reads model output (cmaq default)
 
     if model == 'cmaq':
-    	 da=open_cmaq(finput,verbose=verbose)  
+        da = open_cmaq(finput, verbose=verbose)
     else:
-         print('Must enter cmaq model right now')
-         raise RuntimeError
+        print('Must enter cmaq model right now')
+        raise RuntimeError
 
 #retrieves data observations and formats pandas dataframe (airnow default)
-    start = da.time.to_index().min() - pd.Timedelta(1,unit='H')
-    end = da.time.to_index().max() + pd.Timedelta(1,unit='H')
-    print(start.strftime('%Y%m%d%H'), end.strftime('%Y%m%d%H'))
-    print(start)
-    print(end)
-    df=get_aeronet(start,end)
+    start = da.time.to_index().min() - pd.Timedelta(1, unit='h')
+    end = da.time.to_index().max() + pd.Timedelta(1, unit='h')
+    # print(start.strftime('%Y%m%d%H'), end.strftime('%Y%m%d%H'))
+    # print(start)
+    # print(end)
+    df = get_aeronet(start, end)
     #pairs surface point-type observations with 2D model parameters
-    
-    mapping_table = {'aod_550nm':'pm25aod550'}
-    #sub_map = {i: mapping_table[i] for i in species if i in mapping_table}
-#    print(sub_map)
-    print(mapping_table)
-#    dfpair=pair_point(da,df,sub_map,interp)
-    dfpair=pair_point(da,df,mapping_table,interp)
-#    print(dfpair.head())
-    dfpair.to_hdf(output,'df',format='table')
-         
+
+    mapping_table = {'aod_550nm': 'pm25aod550'}
+    dfpair = pair_point(da, df, mapping_table, interp)
+    #    print(dfpair.head())
+    dfpair.to_hdf(output, 'df', format='table')
+
     sys.exit(0)
-    
-
-
